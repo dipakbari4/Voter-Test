37c4d6797a16348fc4f1f53ed9eb88cca1b43ab9
Dipak Bari <dipakbari4@gmail.com>
2020-07-19 23:27:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Voter_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
